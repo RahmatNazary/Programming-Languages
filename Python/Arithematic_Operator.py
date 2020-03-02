a41e269504b3e4a6bb036dccc8e7fe254194527d
RahmatNazary <rahmat.technology@gmail.com>
2020-03-02 21:39:36 +0430

    Data Types ,Operators, and Variable Assignment

---
+++ Python/Arithematic_Operator.py
@@ -0,0 +1,12 @@
+#Using Arithemtatic Operatos like (+, -, *, / , % , **)
+
+print('Addition:',10 + 20)
+print('Subtraction:', 30 - 20)
+print('Division:', 20/2)
+print('Multiplication:', 20 * 3)
+print('Modulo/Mode',25%3)
+
+#Using many operators together
+print(10+20*2+20)
+print((10+20)*(2+20))
+
